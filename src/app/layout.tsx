d5bcb4880edb28873e581742e47600d72e89a7ff
Cheikh <kheuch2108@gmail.com>
2026-09-22 16:05:39 +0000

    correction build

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,14 +4,14 @@ import './globals.css'
 
 const inter = Inter({
   subsets: ['latin'],
-  weight: ['100', '200', '300', '400'],
   variable: '--font-inter',
+  display: 'swap',
 })
 
 const jetbrainsMono = JetBrains_Mono({
   subsets: ['latin'],
-  weight: ['200', '300'],
   variable: '--font-jetbrains-mono',
+  display: 'swap',
 })
 
 export const metadata: Metadata = {
